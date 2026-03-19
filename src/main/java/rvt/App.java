beb023a63431648f1ee4a3ef906ba2dad3397d5f
A24DP3RAlfe <A240182RA@rvt.lv>
2026-03-19 13:39:54 +0200

    Viss kaut kas

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,20 @@
 package rvt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-    // HIhi haha
+public static void main(String[] args) {
+    File file  = new File("src/main/java/rvt/ToDolist.java");
+    try (Scanner reader = new Scanner(file)) {
+        if (reader.hasNextLine()) {
+            System.out.println(reader.nextLine());
+        } else {
+            System.out.println("File is empty.");
+        }
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
     }
+}
 }

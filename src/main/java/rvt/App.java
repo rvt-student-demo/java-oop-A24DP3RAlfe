12d89a974210746385aeedc599bd1f734267c8bf
A24DP3RAlfe <A240182RA@rvt.lv>
2026-05-07 09:47:42 +0300

    Papildināts App.java

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,20 +1,10 @@
 package rvt;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import rvt.registration.RegistrationCLI;
 
 public class App {
-public static void main(String[] args) {
-    File file  = new File("src/main/java/rvt/ToDolist.java");
-    try (Scanner reader = new Scanner(file)) {
-        if (reader.hasNextLine()) {
-            System.out.println(reader.nextLine());
-        } else {
-            System.out.println("File is empty.");
-        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
+    public static void main(String[] args) {
+        RegistrationCLI cli = new RegistrationCLI();
+        cli.start();
     }
-}
 }

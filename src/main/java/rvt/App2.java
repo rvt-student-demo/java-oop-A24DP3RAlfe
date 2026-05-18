6d21662f1c9be15891421f08d6e2c80c12032755
A24DP3RAlfe <A240182RA@rvt.lv>
2026-05-18 09:22:14 +0300

    Java grafika

---
+++ src/main/java/rvt/App2.java
@@ -0,0 +1,16 @@
+package rvt;
+
+import javax.swing.JFrame;
+
+public class App2 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Grafika");
+        frame.setSize(1024, 768);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+
+        // logs.add();
+    }
+}
